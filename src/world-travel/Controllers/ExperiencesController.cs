b78f9a58065b977089f24bfd2f8329cb418ca029
Jiwon Kang and Tammy Dang <jiwon+tammy@>
2017-04-19 14:44:25 -0700

    add delete functionality

--- src/world-travel/Controllers/ExperiencesController.cs
+++ src/world-travel/Controllers/ExperiencesController.cs
@@ -49,5 +49,20 @@ namespace world_travel.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public IActionResult Delete(int id)
+        {
+            var thisExperience = db.Experiences.FirstOrDefault(experiences => experiences.ExperienceId == id);
+            return View(thisExperience);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisExperience = db.Experiences.FirstOrDefault(experiences => experiences.ExperienceId == id);
+            db.Experiences.Remove(thisExperience);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
